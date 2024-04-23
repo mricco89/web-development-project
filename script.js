6c675101c4b6cd636e094d8fc979bbc8c38ec50e
mricco89 <58084623+mricco89@users.noreply.github.com>
2024-04-22 21:19:09 -0400

    Add files via upload
    added a top of page button to the about page. pretty sure I covered all the bases of what was asked for for the project in the last commit but wanted to add a top button. It didnt come out the way I wanted but i eventually got it to work.

--- script.js
+++ script.js
@@ -85,3 +85,20 @@ function toggleElement(id) {
         element.style.display = 'none';
     }
 }
+
+// Function to scroll to the top of the page
+function scrollToTop() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+// Function to toggle visibility of the top button based on scroll position
+window.onscroll = function() {
+    toggleTopButton();
+};
+
+// Function to toggle visibility of the top button based on scroll position
+function toggleTopButton() {
+    var topButton = document.getElementById("topButton");
+    topButton.style.display = "block"; // Always set display to "block"
+}
